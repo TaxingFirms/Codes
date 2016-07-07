fcbb567c85d79f13970bde467d46ae4bfeae864f
danielwillsr <danielwillsr@gmail.com>
2016-07-06 20:21:21 -0400

    Transitions still under construction

---
+++ SolveSteadyState.jl
@@ -0,0 +1,25 @@
+function SolveSteadyState!(tau::Taxes,pa::Param;wguess::Float64=0.69,
+  VFIfunction::Function = firmVFIParallelOmega!, distr_routine::Function = stationarydist,
+     maxroutine::Function=maximizationstep )
+
+  eq = init_equilibirium(wguess,tau,pa);
+  pr  = init_firmproblem(eq,tau,pa);
+
+  #Compute the model on first time
+  @time VFIfunction(pr,eq,tau,pa; maximizationroutine=maxroutine); #pr is updated, computes Value Function
+
+  #Compute wage such that free entry condition holds
+  @time w=free_entry!(eq, pr, tau, pa, VFIfunction, maxroutine)
+
+  #Extract policies and other idiosyncratic results of interest
+  getpolicies!(pr,eq,tau,pa);  #r is updated exctracts policies
+
+  # Compute mass of entrants and stationary distribution
+  # both are updated in p.
+  mass_of_entrants!( pr, eq, tau, pa, distr_routine);
+
+  # Compute aggregate results of interest and moments
+  aggregates!(pr, eq, tau, pa);
+
+  return pr,eq
+end
---
+++ Transitions.jl
@@ -0,0 +1,53 @@
+# This script takes an initial equilibrium, a final equilibrium and a path for
+# taxes and return a path for equilibrium prices and allocations
+
+type SteadyState
+  fpr::FirmProblem
+  hpr::HouseholdProblem
+  eq::Equilibrium
+end
+
+function init_transitions(T::Int64)
+  transitions= Array(SteadyState,(T,));
+end
+
+  function transitions!(tr:: Array(SteadyState,(T,)), T::Int64, ss0::SteadyState, ssT::SteadyState , tauseq::Array{Taxes,1}, pa::Param; update::Float64=0.9, tol = 10^-4.0, maxit=10^4.0 , maxroutine::Function=maximizationstep )
+
+    # Guess paths for r, w, E
+    for t=1:T-1
+      tr[t].eq.w= t*(ssT.eq.w - ss0.eq.w)/T + ss0.eq.w;
+      tr[t].eq.r= t*(ssT.eq.r - ss0.eq.r)/T + ss0.eq.r;
+      tr[t].eq.E= t*(ssT.eq.E - ss0.eq.E)/T + ss0.eq.E;
+    end
+
+    # Assume new steady state is reached after T periods
+    tr[T] = ssT;
+
+
+
+    # Solve for path for prices using a shooting algorithm
+    while excess>tol  && it<maxit
+
+      #Solve value function backwards
+      for t=T:2
+        pr= deepcopy(tr[t].fpr);
+        firmbellmanParallelOmega!(pr,tr[t].eq,tausec[t],pa,maxroutine);
+        getpolicies!(pr,tr[t].eq,tr[t].tau,pa);
+        tr[t-1].fpr = pr;
+      end
+
+      #Compute cross-sectional distribution forward (this can be made faster if needed)
+      tr[1].eq.distr = transitionrule(ss0.eq.distr, tr[1].eq.E, tr[1].fpr, tr[1].eq , tausec[1], pa);
+      for t=2:T
+        tr[t].eq.distr = transitionrule(tr[t-1].eq.distr,tr[t].eq.E, tr[t].fpr, tr[t].eq , tausec[t], pa);
+      end
+
+      #Check market clearing conditions
+
+
+      #Update prices
+
+    end
+
+
+  end
