9fc86a3b1ffb2e1916da32c1f40a86ed67f7cc62
danielwillsr <danielwillsr@gmail.com>
2016-08-17 15:21:30 -0400

    Tax Reform working apparently well

--- TaxReforms.jl
+++ TaxReforms.jl
@@ -42,6 +42,9 @@ end
 
 function taxreform2(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; update::Float64 =0.7, tol::Float64 =10.0^-2.0, momentsprint::Bool=false, verbose::Bool=false,firmvalueguess::Matrix = repmat(pa.omega.grid,1,pa.Nz) )
 
+#tauc= 0.0; update=0.7; tol= 10.0^-2.0; momentsprint=false; verbose=false; firmvalueguess=copy(pr.firmvaluegrid);
+
+
     #Compute tax base for "revenue neutral" reforms
     C = eq.a.collections.c / tau.c #corporate base
     D = eq.a.collections.d / tau.d #divideend base
@@ -60,7 +63,7 @@ function taxreform2(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; updat
     println("New rates: d = ", taunew.d, " c = ", taunew.c, " i = ", taunew.i, " g = ", taunew.g)
 
     #Initiate prices and firm problem, and ultimately, the counterfactual object.
-    pr1,eq1=SolveSteadyState(taunew,pa; wguess = wguess, verbose=verbose, firmvalueguess = firmvalueguess)
+    pr1,eq1=SolveSteadyState(taunew,pa; wguess = wguess, verbose=verbose, firmvalueguess = firmvalueguess);
     if momentsprint
         moments=computeMomentsCutoff(eq1.E,pr1,eq1,tau,pa,cutoffCapital=0.0,toPrint=momentsprint);
     end
@@ -70,23 +73,24 @@ function taxreform2(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; updat
     newwguess=eq1.w;
 
     while abs((originalG - newG)/originalG)>tol
-        println("(originalG - newG)/originalG", (originalG - newG)/originalG)
-        tau=deepcopy(taunew);
+        println("(originalG - newG)/originalG ", (originalG - newG)/originalG)
+        tauprime=deepcopy(taunew);
 
-        D= eq1.a.collections.d / tau.d;
-        ntau= update*tau.d + (1-update)*(tau.d + (originalG -newG)/D);
+        D= eq1.a.collections.d / tauprime.d;
+        ntau= update*tauprime.d + (1-update)*(tauprime.d + (originalG -newG)/D);
 
-        if (1-tau.c)*(1-ntau)>(1-tau.i)
-            error("No equilibrium under current taxes")
-        end
-        taunew = Taxes(ntau,tau.c,tau.i,ntau,tau.l);
+    #    if (1-tauprime.c)*(1-ntau)>(1-tau.i)
+    #        error("No equilibrium under current taxes")
+    #    end
+        taunew = Taxes(ntau,tauprime.c,tauprime.i,ntau,tau.l);
         println("New rates: d = ", taunew.d, " c = ", taunew.c, " i = ", taunew.i, " g = ", taunew.g)
 
         pr1,eq1=SolveSteadyState(taunew,pa; wguess = newwguess, verbose=verbose, firmvalueguess = newfirmvalueguess);
         newG=eq1.a.G;
         newfirmvalueguess=copy(pr1.firmvaluegrid);
         newwguess=eq1.w;
-        println("(originalG - newG)/originalG",(originalG - newG)/originalG)
+        println("(originalG - newG)/originalG ",(originalG - newG)/originalG)
+        println(abs((originalG - newG)/originalG)>tol)
         return pr1, eq1, taunew
     end
 end
