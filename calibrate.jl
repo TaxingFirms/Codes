acc12d602d525ed26975359cbbde0f82d530163a
danielwillsr <danielwillsr@gmail.com>
2016-09-07 00:30:01 -0400

    no print

--- calibrate.jl
+++ calibrate.jl
@@ -21,7 +21,7 @@ function computeDistance(initialParams)
 	pa  = init_parameters(ddelta=initialParams[1],ttheta=initialParams[2],rhoz=initialParams[3], ssigmaz=initialParams[4],llambda0=initialParams[5],llambda1=initialParams[6],  ff =initialParams[7], H= initialParams[8], e =initialParams[9], Nz=Nnz,Nk=Nnk,Nomega=Nnomega, Nq=Nnq);
 	tau = init_taxes(ttaud =0.15, ttauc= 0.35, ttaui= 0.28, ttaug= 0.15, ttaul=0.28);
 
-	pr,eq= SolveSteadyState(tau,pa, VFItol=10.0^-3.0);
+	pr,eq= SolveSteadyState(tau,pa, VFItol=10.0^-3.0;displayit0=false, displayw=false);
 	moments = computeMomentsCutoff(eq.E,pr, eq, tau, pa, cutoffCapital=0.0, toPrint=false)
 
 	currentMomentsMatch = [moments.mean_inv_rate,moments.sd_profits2k, moments.mean_leverage,
