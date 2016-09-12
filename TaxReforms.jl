32173c9ed34e175d25596f64f8978cb7e2c13b51
danielwillsr <danielwillsr@gmail.com>
2016-09-12 19:54:47 -0400

    tax reforms with new parameters

--- TaxReforms.jl
+++ TaxReforms.jl
@@ -1,5 +1,5 @@
 
-function taxreform1(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; update::Float64 =0.7, tol::Float64 =10.0^-3.0, momentsprint::Bool=false, verbose::Bool=false,firmvalueguess::Matrix = repmat(pa.omega.grid,1,pa.Nz))
+function taxreform1(tauc::Float64,  govexp::Float64, eq::Equilibrium, tau::Taxes, pa::Param; update::Float64 =0.7, tol::Float64 =10.0^-3.0, momentsprint::Bool=false, verbose::Bool=false,firmvalueguess::Matrix = repmat(pa.omega.grid,1,pa.Nz))
 # Gets a new level of tauc. CLoses using the dividend tax
 
   #Compute tax base for "revenue neutral" reforms
@@ -7,14 +7,14 @@ function taxreform1(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; updat
   D = eq.a.collections.d / tau.d
   I = eq.a.collections.i / tau.i
 
-  originalG=eq.a.G;
+  originalG=govexp;
   wguess= eq.w;
 
   x= (tauc - tau.c)*C/D;
   taunew = Taxes(tau.d-x,tauc,tau.i,tau.g,tau.l);
   println("New rates: d = ", taunew.d, " c = ", taunew.c, " i = ", taunew.i, " g = ", taunew.g)
 
-  pr1,eq1=SolveSteadyState(taunew,pa;  wguess = wguess,verbose=false);
+  pr1,eq1=SolveSteadyState(taunew,pa; wguess = wguess, firmvalueguess = pr.firmvaluegrid, displayit0=false, displayw = false);
   if momentsprint
       moments=computeMomentsCutoff(eq1.E,pr1,eq1,tau,pa,cutoffCapital=0.0,toPrint=momentsprint);
   end
@@ -30,20 +30,22 @@ function taxreform1(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; updat
     taunew = Taxes(ntaud,tauprime.c,tauprime.i,tauprime.g,tauprime.l);
     println("New rates: d = ", taunew.d, " c = ", taunew.c, " i = ", taunew.i, " g = ", taunew.g)
 
-    pr1,eq1= SolveSteadyState(taunew,pa; wguess = wguess, verbose=false);
+    initialradius = min(abs(eq1.w-wguess),10.0^-2.0);
+    wguess=eq1.w;
+    pr1,eq1= SolveSteadyState(taunew,pa; wguess = wguess, firmvalueguess = pr1.firmvaluegrid, displayit0=false, displayw = false, initialradius = initialradius);
     if momentsprint
         moments=computeMomentsCutoff(eq1.E,pr1,eq1,tau,pa,cutoffCapital=0.0,toPrint=momentsprint);
     end
 
     newG=eq1.a.G;
   end
-  println("(originalG - newG)/originalG",(originalG - newG)/originalG)
+  println("(originalG - newG)/originalG ",(originalG - newG)/originalG)
  return pr1, eq1, taunew
 end
 
 
 
-function taxreform2(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; update::Float64 =0.7, tol::Float64 =10.0^-3.0, momentsprint::Bool=false, verbose::Bool=false,firmvalueguess::Matrix = repmat(pa.omega.grid,1,pa.Nz) )
+function taxreform2(tauc::Float64, govexp::Float64, eq::Equilibrium, tau::Taxes, pa::Param; update::Float64 =0.7, tol::Float64 =10.0^-3.0, momentsprint::Bool=false,firmvalueguess::Matrix = repmat(pa.omega.grid,1,pa.Nz) )
 # Gets a new level of tauc. CLoses using the dividend and capital gains taxes at the same time.
 
 #tauc= 0.0; update=0.7; tol= 10.0^-2.0; momentsprint=false; verbose=false; firmvalueguess=copy(pr.firmvaluegrid);
@@ -54,7 +56,7 @@ function taxreform2(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; updat
     D = eq.a.collections.d / tau.d #divideend base
     I = eq.a.collections.i / tau.i #interest base
 
-    originalG=eq.a.G;
+    originalG=govexp;
     wguess= eq.w;
 
     x= (tauc - tau.c)*C/D;
@@ -67,7 +69,7 @@ function taxreform2(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; updat
     println("New rates: d = ", taunew.d, " c = ", taunew.c, " i = ", taunew.i, " g = ", taunew.g)
 
     #Initiate prices and firm problem, and ultimately, the counterfactual object.
-    pr1,eq1=SolveSteadyState(taunew,pa; wguess = wguess, verbose=verbose);
+    pr1,eq1=SolveSteadyState(taunew,pa; wguess = wguess, firmvalueguess = pr.firmvaluegrid, displayit0=false, displayw = false);
     if momentsprint
         moments=computeMomentsCutoff(eq1.E,pr1,eq1,tau,pa,cutoffCapital=0.0,toPrint=momentsprint);
     end
@@ -86,7 +88,9 @@ function taxreform2(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; updat
         taunew = Taxes(ntau,tauprime.c,tauprime.i,ntau,tau.l);
         println("New rates: d = ", taunew.d, " c = ", taunew.c, " i = ", taunew.i, " g = ", taunew.g)
 
-        pr1,eq1=SolveSteadyState(taunew,pa; wguess = wguess, verbose=verbose);
+        initialradius = min(abs(eq1.w-wguess),10.0^-2.0);
+        wguess=eq1.w;
+        pr1,eq1=SolveSteadyState(taunew,pa; wguess = wguess , firmvalueguess = pr1.firmvaluegrid, displayit0=false, displayw = false , initialradius = initialradius);
         if momentsprint
             moments=computeMomentsCutoff(eq1.E,pr1,eq1,tau,pa,cutoffCapital=0.0,toPrint=momentsprint);
         end
@@ -98,7 +102,7 @@ end
 
 
 
-function taxreform3(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; update::Float64 =0.7, tol::Float64 =10.0^-3.0, momentsprint::Bool=false, verbose::Bool=false,firmvalueguess::Matrix = repmat(pa.omega.grid,1,pa.Nz) )
+function taxreform3(tauc::Float64, govexp::Float64, eq::Equilibrium, tau::Taxes, pa::Param; update::Float64 =0.7, tol::Float64 =10.0^-3.0, momentsprint::Bool=false, verbose::Bool=false,firmvalueguess::Matrix = repmat(pa.omega.grid,1,pa.Nz) )
 # Gets a new level of tauc. Closes using dividend = capital gains = interest.
 
 
@@ -107,7 +111,7 @@ function taxreform3(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; updat
   D = eq.a.collections.d / tau.d; #dividend base
   I = eq.a.collections.i / tau.i; #interest base
 
-  originalG=eq.a.G;
+  originalG=govexp;
   wguess= eq.w;
 
   tauind= (originalG - tauc*C - eq.a.collections.l)/(D + I);
@@ -116,7 +120,7 @@ function taxreform3(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; updat
 
 
   #Initiate prices and firm problem, and ultimately, the counterfactual object.
-  pr1,eq1=SolveSteadyState(taunew,pa; wguess = wguess, verbose=verbose);
+  pr1,eq1=SolveSteadyState(taunew,pa; wguess = wguess, firmvalueguess = pr.firmvaluegrid, displayit0=false, displayw = false);
   if momentsprint
       moments=computeMomentsCutoff(eq1.E,pr1,eq1,tau,pa,cutoffCapital=0.0,toPrint=momentsprint);
   end
@@ -134,7 +138,9 @@ function taxreform3(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; updat
     taunew = Taxes(ntauind,tauc,ntauind,ntauind,tau.l);
     println("New rates: d = ", taunew.d, " c = ", taunew.c, " i = ", taunew.i, " g = ", taunew.g)
 
-    pr1,eq1=SolveSteadyState(taunew,pa; wguess = wguess, verbose=false);
+    initialradius = min(abs(eq1.w-wguess),10.0^-2.0);
+    wguess=eq1.w;
+    pr1,eq1=SolveSteadyState(taunew,pa; wguess = wguess , firmvalueguess = pr1.firmvaluegrid, displayit0=false, displayw = false , initialradius = initialradius);
     if momentsprint
         moments=computeMomentsCutoff(eq1.E,pr1,eq1,tau,pa,cutoffCapital=0.0,toPrint=momentsprint);
     end
@@ -145,14 +151,14 @@ function taxreform3(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; updat
 end
 
 
-function taxreform4(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; update::Float64 =0.7, tol::Float64 =10.0^-3.0, momentsprint::Bool=false, verbose::Bool=false,firmvalueguess::Matrix = repmat(pa.omega.grid,1,pa.Nz) )
+function taxreform4(tauc::Float64, govexp::Float64, eq::Equilibrium, tau::Taxes, pa::Param; update::Float64 =0.7, tol::Float64 =10.0^-3.0, momentsprint::Bool=false, verbose::Bool=false,firmvalueguess::Matrix = repmat(pa.omega.grid,1,pa.Nz) )
 
   #Compute tax base for "revenue neutral" reforms
   C = eq.a.collections.c / tau.c; #corporate base
   D = eq.a.collections.d / tau.d; #dividend base
   I = eq.a.collections.i / tau.i; #interest base
 
-  originalG=eq.a.G;
+  originalG=govexp;
   wguess= eq.w;
 
   tauinew=1-(1-tau.g)*(1-tauc) - 10.0^-3.0
@@ -194,6 +200,7 @@ end
 
 
 function taxreform5(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; update::Float64 =0.7, tol::Float64 =10.0^-3.0, momentsprint::Bool=false, verbose::Bool=false,firmvalueguess::Matrix = repmat(pa.omega.grid,1,pa.Nz) )
+## SAME AS REFORM 3, BUT THE EXTRA REVENUE FROM THE
 
   #Compute tax base for "revenue neutral" reforms
   C = eq.a.collections.c / tau.c; #corporate base
@@ -231,52 +238,7 @@ function taxreform5(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; updat
     if momentsprint
         moments=computeMomentsCutoff(eq1.E,pr1,eq1,tau,pa,cutoffCapital=0.0,toPrint=momentsprint);
     end
-    newG=eq1.a.G - eq1.a.collections.l ;;
-  end
-  println("(originalG - newG)/originalG ",(originalG - newG)/originalG)
-  return pr1, eq1, taunew
-end
-
-
-function taxreform6(tauc::Float64, eq::Equilibrium, tau::Taxes, pa::Param; update::Float64 =0.7, tol::Float64 =10.0^-3.0, momentsprint::Bool=false, verbose::Bool=false,firmvalueguess::Matrix = repmat(pa.omega.grid,1,pa.Nz) )
-
-  #Compute tax base for "revenue neutral" reforms
-  C = eq.a.collections.c / tau.c; #corporate base
-  D = eq.a.collections.d / tau.d; #dividend base
-  I = eq.a.collections.i / tau.i; #interest base
-
-  originalG=eq.a.G - 0.7*eq.a.collections.l;
-  wguess= eq.w;
-
-  tauind= (originalG - tauc*C - 0.3*eq.a.collections.l)/(D + I);
-  taunew = Taxes(tauind,tauc,tauind,tauind,tau.l);
-  println("New rates: d = ", taunew.d, " c = ", taunew.c, " i = ", taunew.i, " g = ", taunew.g)
-
-
-  #Initiate prices and firm problem, and ultimately, the counterfactual object.
-  pr1,eq1=SolveSteadyState(taunew,pa; wguess = wguess, verbose=verbose);
-  if momentsprint
-      moments=computeMomentsCutoff(eq1.E,pr1,eq1,tau,pa,cutoffCapital=0.0,toPrint=momentsprint);
-  end
-  newG=eq1.a.G- 0.7*eq.a.collections.l;;
-
-  while abs((originalG - newG)/originalG)>tol
-    println("(originalG - newG)/originalG ",(originalG - newG)/originalG)
-    tauprime=deepcopy(taunew);
-
-    D= eq1.a.collections.d / tauprime.d;
-    I= eq1.a.collections.i / tauprime.i;
-    tauind= (originalG - tauc*C - 0.3*eq1.a.collections.l)/(D + I);
-    ntauind= update*tauprime.d + (1-update)*tauind;
-
-    taunew = Taxes(ntauind,tauc,ntauind,ntauind,tau.l);
-    println("New rates: d = ", taunew.d, " c = ", taunew.c, " i = ", taunew.i, " g = ", taunew.g)
-
-    pr1,eq1=SolveSteadyState(taunew,pa; wguess = wguess, verbose=false);
-    if momentsprint
-        moments=computeMomentsCutoff(eq1.E,pr1,eq1,tau,pa,cutoffCapital=0.0,toPrint=momentsprint);
-    end
-    newG=eq1.a.G- 0.7*eq.a.collections.l;;
+    newG=eq1.a.G - eq1.a.collections.l ;
   end
   println("(originalG - newG)/originalG ",(originalG - newG)/originalG)
   return pr1, eq1, taunew
--- running.jl
+++ running.jl
@@ -16,27 +16,25 @@ include("mc_tools.jl")
 @everywhere include("Distribution.jl")
 @everywhere include("Aggregation.jl")
 @everywhere include("SolveSteadyState.jl")
-@everywhere include("TaxReforms.jl")
 @everywhere include("calibrate.jl")
 @everywhere include("Transitions.jl")
 
 
-include("Simulations.jl")
-include("Magnitudes.jl")
-include("PlotFunctions.jl")
-include("Reforms.jl")
 
-
-
-pa =init_parameters(H=1.07, ddelta=0.125,ttheta=0.2, llambda1 = 0.2, rhoz = 0.75, ssigmaz = 0.072  ,ff= 0.5, e= 0.16);
-
-#pa =init_parameters(H = 1.2135, ddelta=0.0768, ttheta = 0.274 , rhoz =0.75, ssigmaz = 0.086, llambda0 = 0.03, llambda1 = 0.1507, ff = 0.796, e=0.0388);
-
-#tesla
-#pa =init_parameters(ddelta = 0.06952976680384088, ttheta= 0.31405486968449925, rhoz= 0.9277777777777777, ssigmaz= 0.059415637860082304, llambda0= 0.07196200274348423, llambda1= 0.049990855052583445, f= 0.9707050754458164, e= 0.025813443072702332)
+pa =init_parameters(H = 1.2135, ddelta=0.0768, ttheta = 0.14067 , rhoz =0.75, ssigmaz = 0.086, llambda0 = 0.0122, llambda1 = 0.1507, ff = 1.3267, e=0.073289);
 tau = init_taxes(ttaud =0.15, ttauc= 0.35, ttaui= 0.28, ttaug= 0.15, ttaul=0.28);
 @time pr,eq= SolveSteadyState(tau,pa;wguess=0.556, VFItol=10.0^-3.0, displayit0=true, displayw = true);
 moments=computeMomentsCutoff(eq.E,pr,eq,tau,pa,cutoffCapital=0.0;toPrint=false);
 pcterror_params(pr,eq,tau,pa)
 save("ModelResults.jld","pr",pr,"eq",eq,"tau",tau,"pa",pa);
 #pr,eq,tau,pa=load("ModelResults.jld", "pr","eq","tau","pa");
+
+
+#####################################################################
+include("TaxReforms.jl")
+include("Reforms.jl")
+
+taucvec = [0.33 0.30 0.27 0.24 0.21 0.18 0.15 0.12 0.09 0.06 0.03 0.00];
+reform2 = Reform2Vector("reform2.jld", taucvec, pr, eq, tau, pa)
+reform1 = Reform1Vector("reform1.jld", taucvec, pr, eq, tau, pa)
+reform3 = Reform3Vector("reform3.jld", taucvec, pr, eq, tau, pa)
