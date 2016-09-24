be7d7ff052c883944d09ac337e8354d46e98e583
danielwillsr <danielwillsr@gmail.com>
2016-09-24 11:59:33 -0400

    f

--- runTaxReforms.jl
+++ runTaxReforms.jl
@@ -23,8 +23,27 @@ pr,eq,tau,pa=load("ModelResults.jld", "pr","eq","tau","pa");
 
 taucvec = [0.33 0.31 0.29 0.27 0.25 0.23 0.21 0.19 0.17 0.15 0.13 0.11 0.09 0.07 0.05 0.03 0.01 0.00];
 ref2 = Reform2Vector("reform2.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-4.0, update=0.75);
-ref3 = Reform3Vector("reform3.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-4.0, update=0.75);
+
+taucvec = [0.35 0.33 0.31 0.29 0.27 0.25 0.23 0.21 0.19 0.17 0.15 0.13 0.11 0.09 0.07 0.05 0.03 0.01 0.00];
+
+tau.i=0.3;
+ref2 = Reform2Vector("reform2_03.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-4.0, update=0.75);
+tau.i=0.2;
+ref2 = Reform2Vector("reform2_02.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-4.0, update=0.75);
+tau.i=0.1;
+ref2 = Reform2Vector("reform2_01.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-4.0, update=0.75);
+tau.i=0.0;
+ref2 = Reform2Vector("reform2_00.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-4.0, update=0.75);
+
+taucvec = [0.33 0.31 0.29 0.27 0.25 0.23 0.21 0.19 0.17 0.15 0.13 0.11 0.09 0.07 0.05 0.03 0.01 0.00];
 ref1 = Reform1Vector("reform1.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-4.0, update=0.75);
+
+
+taucvec = [0.33 0.31 0.29 0.27 0.25 0.23 0.21 0.19 0.17 0.15 0.13 0.11 0.09 0.07 0.05 0.03 0.01 0.00];
+ref1 = Reform1Vector("reform1.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-4.0, update=0.75);
+
+taucvec = [0.33 0.31 0.29 0.27 0.25 0.23 0.21 0.19 0.17 0.15 0.13 0.11 0.09 0.07 0.05 0.03 0.01 0.00];
+ref3 = Reform3Vector("reform3.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-4.0, update=0.75);
 
 tauivec = [0.27 0.25 0.23 0.21 0.19 0.17 0.15 0.13 0.11 0.09 0.07 0.05 0.03 0.01 0.00 ];
 re2taui = Reform2TauIVector("Reform2TauIVector.jld", tauivec, pr, eq, tau, pa; bctol= 5.0*10.0^-4.0, update=0.75);
