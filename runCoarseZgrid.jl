a4748d5c8b27dacb21d8b4b50b7be34bed7d558d
danielwillsr <danielwillsr@gmail.com>
2016-10-08 19:11:50 -0400

    m

--- runCoarseZgrid.jl
+++ runCoarseZgrid.jl
@@ -27,6 +27,7 @@ save("ResultsCoarseZgrid.jld","pr",pr,"eq",eq,"tau",tau,"pa",pa);
 include("TaxReforms.jl")
 include("Reforms.jl")
 
+
 taucvec = [0.33 0.31 0.29 0.27 0.25 0.23 0.21 0.19 0.17 0.15 0.13 0.11 0.09 0.07 0.05 0.03 0.01 0.00];
 ref5 = Reform5Vector("ref5CoarseZgrid.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-3.0, update=0.75);
 #ref1,pa=load("ref1CoarseZgrid.jld","ref","pa");
