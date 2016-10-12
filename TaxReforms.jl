c4e55c9894965d07b6631f1feaa2d141cdd4e9ba
danielwillsr <danielwillsr@gmail.com>
2016-10-11 20:21:41 -0400

    m

--- TaxReforms.jl
+++ TaxReforms.jl
@@ -336,7 +336,7 @@ function taxreform5(tauc::Float64, govexp::Float64, pr::FirmProblem, eq::Equilib
 
   #Initiate prices and firm problem, and ultimately, the counterfactual object.
   pr1,eq1=SolveSteadyState(taunew,pa; wguess = wguess, firmvalueguess = pr.firmvaluegrid, displayit0=false, displayw = false);
-  println(@sprintf(" Dividend Base \t Dividend Prev D \t  Consumption \t Mass Entrants \t     Wage    \t    Welfare  \t    TFP   \t       G     "))
+  println(@sprintf(" Dividend Base \t DivPrevDis \t  Consumption \t Mass Entrants \t     Wage    \t    Welfare  \t    TFP   \t       G     "))
   println(@sprintf("%9.4f \t %9.4f \t %9.4f \t %9.4f \t %9.4f  \t %9.4f \t %9.4f  \t %9.4f ", eq1.a.collections.d/taunew.d, sum(eq.distr.*pr1.grossdividends), eq1.a.consumption, eq1.E, eq1.w, eq1.a.welfare , eq1.a.output/(eq1.a.capital^pa.alphak*eq1.a.laborsupply^pa.alphal), eq1.a.G))
   if momentsprint
       moments=computeMomentsCutoff(eq1.E,pr1,eq1,tau,pa,cutoffCapital=0.0,toPrint=true);
@@ -362,7 +362,7 @@ function taxreform5(tauc::Float64, govexp::Float64, pr::FirmProblem, eq::Equilib
     initialradius = min(abs(eq1.w-wguess),10.0^-2.0);
     wguess=eq1.w;
     pr1,eq1=SolveSteadyState(taunew,pa; wguess = wguess , firmvalueguess = pr1.firmvaluegrid, displayit0=false, displayw = false , initialradius = initialradius);
-    println(@sprintf(" Dividend Base \t Dividend Prev D \t  Consumption \t Mass Entrants \t     Wage    \t    Welfare  \t    TFP   \t       G     "))
+    println(@sprintf(" Dividend Base \t DivPrevDis \t  Consumption \t Mass Entrants \t     Wage    \t    Welfare  \t    TFP   \t       G     "))
     println(@sprintf("%9.4f \t %9.4f \t %9.4f \t %9.4f \t %9.4f  \t %9.4f \t %9.4f  \t %9.4f ", eq1.a.collections.d/taunew.d, sum(eq.distr.*pr1.grossdividends), eq1.a.consumption, eq1.E, eq1.w, eq1.a.welfare , eq1.a.output/(eq1.a.capital^pa.alphak*eq1.a.laborsupply^pa.alphal), eq1.a.G))
 
     if momentsprint
--- runZeroTauI.jl
+++ runZeroTauI.jl
@@ -16,14 +16,14 @@ include("mc_tools.jl")
 @everywhere include("Transitions.jl")
 
 #Tesla
-pa =init_parameters(H = 3.4, psi=0.5, ddelta = 0.0769, ttheta = 0.22956 , rhoz = 0.75, ssigmaz = 0.10608, llambda0 = 0.0456, llambda1 = 0.08897, ff = 1.3267, e=0.0260);
+#pa =init_parameters(Nz= 15, H = 3.4, psi=0.5, ddelta = 0.0769, ttheta = 0.22956 , rhoz = 0.75, ssigmaz = 0.10608, llambda0 = 0.0456, llambda1 = 0.08897, ff = 1.3267, e=0.0260);
 
-tau = init_taxes(ttaud = 0.15, ttauc = 0.35, ttaui = 0.0, ttaug = 0.0, ttaul = 0.28);
-@time pr,eq= SolveSteadyState(tau,pa;wguess=0.5, VFItol=10.0^-5.0, displayit0=true, displayw = true);
-moments=computeMomentsCutoff(eq.E,pr,eq,tau,pa,cutoffCapital=0.0;toPrint=true);
+#tau = init_taxes(ttaud = 0.15, ttauc = 0.35, ttaui = 0.0, ttaug = 0.0, ttaul = 0.28);
+#@time pr,eq= SolveSteadyState(tau,pa;wguess=0.5, VFItol=10.0^-5.0, displayit0=true, displayw = true);
+#moments=computeMomentsCutoff(eq.E,pr,eq,tau,pa,cutoffCapital=0.0;toPrint=true);
 #save("ResultsZeroTauI.jld","pr",pr,"eq",eq,"tau",tau,"pa",pa);
 
-#pr,eq,tau,pa =load("ResultsZeroTauI.jld","pr","eq","tau","pa");
+pr,eq,tau,pa =load("ResultsZeroTauI.jld","pr","eq","tau","pa");
 
 include("TaxReforms.jl")
 include("Reforms.jl")
