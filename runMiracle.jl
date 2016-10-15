88ffa95275f67a465a14d8fead872878beeec655
danielwillsr <danielwillsr@gmail.com>
2016-10-15 07:35:29 -0400

    m

--- runMiracle.jl
+++ runMiracle.jl
@@ -31,9 +31,9 @@ include("Reforms.jl")
 tau.i=0.0;
 taucvec = [0.33 0.31 0.29 0.27 0.25 0.23 0.21 0.19 0.17 0.15 0.13 0.11 0.09 0.07 0.05 0.03 0.01 0.00];
 
-ref1 = Reform1Vector("reform1ZeroTauI.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-4.0, update=0.75);
-ref2 = Reform2Vector("reform2ZeroTauI.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-4.0, update=0.75);
-ref3 = Reform3Vector("reform3ZeroTauI.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-4.0, update=0.75);
+ref1 = Reform1Vector("reform1Miracle.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-3.0, update=0.75);
+ref2 = Reform2Vector("reform2Miracle.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-4.0, update=0.75);
+ref3 = Reform3Vector("reform3Miracle.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-4.0, update=0.75);
 
 tau.i=0.28;
 ref2 = Reform2Vector("reform2.jld", taucvec, pr, eq, tau, pa; bctol= 5*10.0^-4.0, update=0.75);
